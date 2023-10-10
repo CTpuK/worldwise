48bf0095025b5696a14da9000fd2ed29d913874d
Evgeny Strikunov <strik.jenya@gmail.com>
2023-10-10 09:58:48 +0300

    first commit

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState, useEffect } from 'react';
+
+const BASE_URL = 'http://localhost:8000';
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert('The was an error loading data...');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+}
